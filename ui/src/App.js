3be47b347d42bceb97d507d0fe5ff0d2d186292b
k-2tha-brimm <kev.brimm@gmail.com>
2019-12-05 16:43:16 -0800

    set up form for login. fix cors issue and confirm authentication

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {f
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    return await fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => { return res.json() })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label /> Email
+          <input type="text"
+                 value={this.state.email}
+                 onChange={this.update('email')}/>
+  
+          <label /> Password
+          <input type="password"
+                 value={this.state.password}
+                 onChange={this.update('password')}/>
+        <input className="session-submit" type="submit" value={this.props.formType} />
+        </form>
+      </div>
+    );
+  }
+}
